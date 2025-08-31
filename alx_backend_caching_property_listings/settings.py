d7c2a235d1ff14f10d9d0a709388e5a0dfb4d834
Eddy Ckedar <eddykedar@gmail.com>
2025-08-31 14:05:36 +0200

    Create settings.py

---
+++ alx_backend_caching_property_listings/settings.py
@@ -0,0 +1,25 @@
+INSTALLED_APPS = [
+    ...,
+    "properties",
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'property_db',
+        'USER': 'property_user',
+        'PASSWORD': 'property_pass',
+        'HOST': 'postgres',  # service name from docker-compose
+        'PORT': '5432',
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
