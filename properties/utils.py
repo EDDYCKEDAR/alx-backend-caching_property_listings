1fecee17f89af6e00de24b860876d8153335e463
Eddy Ckedar <eddykedar@gmail.com>
2025-08-31 14:15:45 +0200

    Update utils.py

--- properties/utils.py
+++ properties/utils.py
@@ -1,15 +1,10 @@
-from django_redis import get_redis_connection
+from django.core.cache import cache
+from .models import Property
 
-def get_redis_cache_metrics():
-    conn = get_redis_connection("default")
-    info = conn.info("stats")
-    hits = info.get("keyspace_hits", 0)
-    misses = info.get("keyspace_misses", 0)
-    total = hits + misses
-    hit_ratio = hits / total if total > 0 else 0
 
-    return {
-        "hits": hits,
-        "misses": misses,
-        "hit_ratio": hit_ratio,
-    }
+def get_all_properties():
+    properties = cache.get('all_properties')
+    if not properties:
+        properties = list(Property.objects.all().values())
+        cache.set('all_properties', properties, 3600)  # cache for 1 hour
+    return properties
